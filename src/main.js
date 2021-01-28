97d115a654311d4fb3df293c9c0a5cd3f76d2f1f
dpgraham <dpgraham.85@gmail.com>
2021-01-27 22:40:00 -0800

    Documentation

--- src/main.js
+++ src/main.js
@@ -24,8 +24,7 @@ async function main () {
         const bundledTestCafePath = path.join(__dirname, '..', 'node_modules', '.bin', 'testcafe');
         const testCafePath = bundledTestCafePath; // Add flag that lets user point to binary on their machine
         const testCafeArgs = process.argv.slice(2); // Change this to 1 when it's a binary
-        const remotePort = 4444; // TODO: Use some type of portfinder
-        const testCafeProc = new SubProcess(testCafePath, ['remote', ...testCafeArgs, '--port', remotePort]);
+        const testCafeProc = new SubProcess(testCafePath, ['remote', ...testCafeArgs]);
         testCafeProc.start(); // TODO: Add a check for testCafe being ready
         testCafeProc.on('lines-stdout', (lines) => {
           for (let line of lines) {
@@ -38,10 +37,10 @@ async function main () {
         });
         testCafeProc.on('output', (stdout, stderr) => {
           if (stdout) {
-            console.log(stdout);
+            console.log(stdout.trim());
           }
           if (stderr) {
-            console.error(stderr);
+            console.error(stderr.trim());
           }
         });
         testCafeProc.on('exit', (code) => {
@@ -49,32 +48,43 @@ async function main () {
           if (code !== 0) {
             reject();
           }
+          session.set;
+          // TODO: Report the status
         });
         return testCafeProc;
       });
     }
     await startTestCafe();
 
+    // TODO: Add command line flags that pull this in
     const capabilities = {
-        browserName: 'Safari',
-        //browserVersion: '13.1',
-        appiumVersion: '1.19.2',
-        platformName: 'iOS',
-        platformVersion: '13.4',
-        deviceName: 'iPhone Simulator',
-        //'sauce:options': {}
+      browserName: process.env.SAUCE_BROWSER_NAME || 'Chrome',
+      browserVersion: process.env.SAUCE_BROWSER_VERSION || 'latest',
+      platformName: process.env.SAUCE_PLATFORM_NAME || 'windows',
+      'sauce:options': {
+        build: process.env.SAUCE_BUILD,
+        name: 'Test Name', // TODO: Parameterize this; make a sensible default
+      }
     };
+    if (process.env.SAUCE_PLATFORM_VERSION) {
+      capabilities.platformVersion = process.env.SAUCE_PLATFORM_VERSION;
+    }
+    if (process.env.SAUCE_DEVICE_NAME) {
+      capabilities.deviceName = process.env.SAUCE_DEVICE_NAME;
+    }
     session = await webdriverio.remote({
         port: 80,
         protocol: 'http',
-        region: 'us', // TODO: Parameterize this
+        region: process.env.SAUCE_REGION || 'us',
         user: process.env.SAUCE_USERNAME,
         key: process.env.SAUCE_ACCESS_KEY,
         capabilities,
     });
     session.url(connectUrl);
+    console.log('Running test on Sauce Cloud'); // TODO: Winstonize this
   } catch (e) {
     console.log(e);
+    process.exit(1);
   }
 };
 
